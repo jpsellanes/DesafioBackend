bedb860f38210e812dfbe804e7bfae3698ffb0a4
jpsellanes <sellanes_juan_pablo@hotmail.com>
2022-05-09 01:13:54 -0300

    Desafio05V01

---
+++ Desafio05/server.js
@@ -0,0 +1,37 @@
+const express = require("express")
+//const bodyParser = require('body-parser')
+const {Router} = express
+const contenedor = require('./main.js')
+const Newcontenedor = new contenedor('productos.json')
+const app = express()
+
+//APP
+app.use(express.static("public"))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.set('view engine', 'ejs')
+
+
+//Router
+const routerProductos = new Router()
+routerProductos.get('/', async(req, res)=>{
+    productos = await Newcontenedor.getAll()
+    res.render('inicio', {productos})
+})
+routerProductos.post('/productos', async(req, res)=>{
+    await Newcontenedor.save(req.body)
+    productos.push(req.body)
+    res.redirect('/')
+})
+
+
+// Carga de router
+app.use("/", routerProductos)
+
+
+//Levantar Server
+const PORT = 8080
+const server = app.listen(PORT, ()=>{
+    console.log(`servidor escuchando ${server.address().port}`)
+})
+server.on("error", error => console.log("error" + error))
