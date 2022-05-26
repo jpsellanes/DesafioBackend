8cf8157c641ed2ffc3f7927933cb4314d9eaa1ec
jpsellanes <sellanes_juan_pablo@hotmail.com>
2022-05-26 01:03:37 -0300

    Desafio06V09

--- Desafio06/server.js
+++ Desafio06/server.js
@@ -15,47 +15,38 @@ const messages = []
 
 //APP
 app.use(express.static("public"))
-app.use(express.urlencoded({ extended: true }))
+//app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.set('view engine', 'ejs')
 
 
 //Router
 const routerProductos = new Router()
-routerProductos.get('/', async(req, res)=>{
-    productos = await Newcontenedor.getAll()
-    res.render('inicio', {productos})
+app.get('/',(req, res)=>{
+    //productos = await Newcontenedor.getAll()
+    //res.render('inicio', {productos})
+    res.render("inicio")
 })
 
-routerProductos.post('/productos', async(req, res)=>{
-    await Newcontenedor.save(req.body)
-    productos = await Newcontenedor.getAll()
-    productos.push(req.body)
-    //res.redirect('/')
-})
-routerProductos.get('/productos', async(req, res)=>{
-    productos = await Newcontenedor.getAll()
-    res.render('productos', {productos})
+routerProductos.get('/productos', async function(req, res){
+    res.json(await Newcontenedor.getAll())
 })
 
 //io socket on
 io.on('connection', async(socket)=>{
     console.log("user connected " + socket.id)
+    
     socket.emit('messages', messages)
     socket.on('new-message', data =>{
         messages.push(data)
-        console.log(typeof(data))
         io.sockets.emit('messages', messages)
-        console.log("aca estaria andnado el chat")
         NewmsgContenedor.save(data)
     })
-    const productos = await Newcontenedor.getAll()
-    socket.emit("productos", productos)
-    socket.on("new-product", productosNuevos =>{
-        //let productosNuevos =  await Newcontenedor.getAll()
-        socket.emit("productos", productosNuevos)
-        io.sockets.emit("productos", productosNuevos)
-        console.log("aca estaria andando el new-product")
+
+    socket.emit("productos")
+    socket.on("new-product", async(data)=>{
+        await Newcontenedor.save(data)
+        io.sockets.emit("productos")
     })
 })
 
